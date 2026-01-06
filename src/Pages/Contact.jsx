da33948fb74300db40676c6ae21f1369f75a1fda
DavidCaaveral <dcanaveral4.5+1@gmail.com>
2026-01-06 18:19:46 -0500

    end of forntend in contact section

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Contact() {
   return (
     <>
     <div className="contact__conatiner">
-      <Header/>
-      <section className="contact__Information__Container">
-        <div className="disclaimer">
-          
+      <section className="contact__Background">
+        <div className="disclaimer__colorBackground">
+          <div className="disclaimer_info">
+            <Header/>
+              <section className="contact__title">
+                <h1 className="contact__title__text">Contacto</h1>
+              </section>
+          </div>
+          <div className="info__contactconatiner">
+            <div className="infocontact_content">
+                <div className="info__contact__text">
+                   <h3 className='textContact'> Puedo ser contactado a traves de mis redes sociales.</h3>
+                   <h5 className='belowTextContact'>Ya mi princesa tiene mi WhatsApp</h5>
+                </div>
+              <i className="fa-solid fa-arrow-down arrowIcon"></i>
+              <section className="socialmedia__container">
+              <i className="fa-brands fa-instagram Socialmedia"></i>
+              <i className="fa-brands fa-linkedin Socialmedia"></i>
+              <i className="fa-brands fa-github Socialmedia"></i>
+              <i className="fa-solid fa-envelope Socialmedia"></i>
+              </section>
+            </div>
+          </div>
+    <Footer />
         </div>
       </section>
     </div>
